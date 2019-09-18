037f6753ea71f28e12a0ac4c71d30b6dcf8cb35b
就看着你们浪 <401328080@qq.com>
2019-09-18 15:12:43 +0800

    file update

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,13 @@
 
 
-function init(){
+function init() {
     let myImage = document.querySelector('img');
     myImage.onclick = function () {
+        if (myImage == null) {
+            return;
+        }
         let mySrc = myImage.getAttribute('src');
+
         if (mySrc === 'images/firefox-icon.png') {
             myImage.setAttribute('src', 'images/firefox2.png');
         } else {
@@ -12,22 +16,22 @@ function init(){
     }
 }
 
-function setHeading(name){
-    document.querySelector('h1').textContent='Mozilla 酷毙了，' + name + '！';
+function setHeading(name) {
+    document.querySelector('h1').textContent = 'Mozilla 酷毙了，' + name + '！';
 }
 function setUserName() {
     let myName = prompt('请输入你的名字');
     localStorage.setItem('name', myName);
     setHeading(myName);
-  } 
+}
 //按钮点击
-function btnClick(){
+function btnClick() {
     let storedName = localStorage.getItem('name');
-    if(!storedName) {
-       setUserName();
+    if (!storedName) {
+        setUserName();
     } else {
-       setHeading(storedName);
+        setHeading(storedName);
     }
-    let myButton = document.querySelector('button'); 
+    let myButton = document.querySelector('button');
     myButton.onclick = setUserName;
 }
